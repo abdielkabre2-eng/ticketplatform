4c71083facc6d2e6e79cbdc690dc47e0a249ed6b
user <user@192.168.11.100>
2026-08-18 13:59:15 +0000

    Mise à jour des liens Vercel, de la page d'achat et nettoyage

--- supabase.js
+++ supabase.js
@@ -1,10 +1,8 @@
+// supabase.js
 const SUPABASE_URL = "https://didtpgzelaqlwbrujqdv.supabase.co";
-
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpZHRwZ3plbGFxbHdicnVqcWR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2NzA1MjYsImV4cCI6MjEwMTI0NjUyNn0.DRaDzyuWqtTRA7AMWUambDWQ9tP16G0ckzxwWRVxj6k";
 
-window.supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
+// On définit 'supabase' comme une variable globale simple
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-console.log("Connexion Supabase chargée :", window.supabaseClient);
+console.log("Connexion Supabase chargée :", supabase);
